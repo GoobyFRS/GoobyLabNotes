b3a8ae58c920f299a97b71b2bf062e51e3d88d61
Maattt GoobyFRS <68407017+GoobyFRS@users.noreply.github.com>
2026-04-27 18:15:31 -0700

    Update homemc_repo_update.sh

--- homemc_repo_update.sh
+++ homemc_repo_update.sh
@@ -1,20 +1,27 @@
 #!/usr/bin/env sh
 
-SERVICE=" minecraft_server"  # ← confirm this matches your actual service name
-
+SERVER_ROOT_PATH="/home/matt/HomeMC/"
+START_PATH=$SERVER_ROOT_PATH/start.sh
+STOP_PATH=$SERVER_ROOT_PATH/stop.sh
+SERVICE="minecraft_server"  # ← confirm this matches your actual service name
+ 
 echo "Starting update..."
-
+ 
 cd ~/HomeMC || { echo "Failed to cd into ~/HomeMC"; exit 1; }
-
+ 
 sudo systemctl stop "$SERVICE"
 echo "Server stopped."
-
+ 
 git stash
 git pull origin main
 echo "Successfully pulled from repo."
-
+ 
 git stash pop
 echo "Stash popped!"
-
+ 
+sudo chmod +x $START_PATH
+sudo chmod +x $STOP_PATH
+echo "Scripts marked executable."
+ 
 sudo systemctl start "$SERVICE"
 echo "Server back online. Done."
